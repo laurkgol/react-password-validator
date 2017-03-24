e1d2556d440e2a0b991b55da667807c1a7696d4c
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-24 15:18:26 -0400

    validation works

--- src/Validator.js
+++ src/Validator.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor(props){
+    super()
+    this.state={
+    password1: props.password1
+    }
+  }
+  passwordClick(e){
+    let firstPassword =  document.getElementById("password1").value
+    let secondPassword =  document.getElementById("password2").value
+    if(firstPassword === secondPassword){
+      alert("Your passwords match!")
+    } else{
+      alert("Your passwords don't match!")
+    }
+  }
   render() {
     return (
       <div className="form">
         <h1>Sign Up</h1>
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="confirm password" />
-        <input type="submit" value="Submit" />
+        <input type="text" placeholder="email"/>
+        <input type="password" placeholder="password" id="password1" value={this.state.password1}/>
+        <input type="password" placeholder="confirm password"id="password2" value={this.state.password1} />
+        <input onClick={(e)=> this.passwordClick(e)}type="submit" value="Submit" />
       </div>
     );
   }
